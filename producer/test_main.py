bdc835eb5adb3950781dd7139d6570f8b8690dee
haohao1029 <ganjinghao2000@gmail.com>
2023-05-11 16:39:20 +0800

    :white_check_mark: (remove standalone test and mo…

---
+++ producer/test_main.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+import json
+import random
+import string
+from datetime import datetime
+
+from .main import app
+
+client = TestClient(app)
+
+url = "http://localhost:8000/process"  # Assuming the producer is running inside the Docker network
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
+def generate_random_request(count):
+
+    # Generate random data for the request body
+    request_data = {
+        'device_id': generate_random_string(10),
+        'client_id': generate_random_string(10),
+        'created_at': datetime.now().isoformat(),
+        "data": {
+            "license_id": "license_id",
+            "preds": []
+        }
+    }
+
+    # Generate random predictions
+    preds_per_message = random.randint(1, 10)
+    for i in range(preds_per_message):
+        prediction = {
+            "image_frame": f"image_frame_{i}_{count}",
+            "prob": random.uniform(0, 1),
+            "tags": []
+        }
+        request_data["data"]["preds"].append(prediction)
+        
+    return request_data
+
+def send_requests():
+
+    for i in range(1000):
+        request_data = generate_random_request(i)
+        headers = {'Content-Type': 'application/json'}
+        response = client.post("/process", data=json.dumps(request_data), headers=headers)
+        assert response.status_code == 200
+
+def test_main():
+    send_requests()
+
+
+if __name__ == "__main__":
+    test_main()
